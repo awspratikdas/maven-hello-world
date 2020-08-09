97bd86f3f26536dafc692b2d94e8335695778192
Pratik Das <pratik.das@hotmail.com>
2020-08-08 20:42:22 -0700

    Create deploymentDynamic.groovy

---
+++ deploymentDynamic.groovy
@@ -0,0 +1,46 @@
+def listProjectName = ["dev", "stage", "prod"]
+
+def disableProj = [
+"dev": true,
+    "stage": false,
+    "prod": true
+]
+
+listProjectName.eachWithIndex { projName, idx ->
+    def jobName = "sample_${projName}"
+freeStyleJob(jobName) {
+    
+     description('My encode decode freestyle job in Groovy')
+    
+    
+    logRotator {
+        numToKeep(5)
+        daysToKeep(7)
+    }
+    disabled(disableProj[projName])
+    
+    parameters {
+        stringParam('env', '${projName}', 'enter the env name')
+    }
+    
+    concurrentBuild(true)
+    
+    scm {
+        git {
+            remote {
+                
+                url('https://github.com/awspratikdas/jenkins-git-integration.git')
+                credentials('test')
+            }
+            
+            branch('master')
+        }
+    }
+    
+    wrappers {
+        credentialsBinding {
+            string('MY_SECRET', 'India DevOps secret text')
+        }
+    }
+}
+}
